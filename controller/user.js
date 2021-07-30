a530c35ab3d03d3ecc3690f4c7888c4083a51a97
Moises Camacho <moisescamacho26@gmail.com>
2021-07-30 13:27:31 -0400

    user validation with password

--- controller/user.js
+++ controller/user.js
@@ -1,12 +1,5 @@
 const User = require('../data/user');
 
-let userl = {
-	username: 'admin',
-	password: '1234',
-	firstName: 'Moises',
-	lastName: 'Camacho',
-	token: 'test',
-};
 exports.List = (req, res) => {
 	User.List((err, data) => {
 		if (err)
@@ -23,26 +16,36 @@ exports.Authenticate = (req, res, next) => {
 		user1: req.body.username,
 		pwd1: req.body.password,
 	};
-	User.Autenticate(data, (error, results) => {
+	User.Find(data, (error, results) => {
 		if (error) {
-			console.log(error);
+			return res.status(400).json({ statusCode: 400, message: 'Error in the database' });
+		}
+		if (results[0].length<1) {
 			return res.status(400).json({ statusCode: 400, message: 'User not exist' });
 		}
-		const data = results[0];
-		if (data[0]) {
-			const userfl = {
-				firstName: data[0].PrimerNombre,
-				lastName: data[0].PrimerApellido,
-				password: 'secrect',
-				token: 'token',
-			};
-			return res.json(userfl);
-		}
-		// return res.status(200).send({
-		// 	success: 1,
-		// 	data: results
-		//
-		// });
-		return res.status(400).json({ statusCode: 400, message: 'User not exist' });
+		console.log('userVerified');
+		
+		User.Autenticate(data, (error, results) => {
+			if (error) {
+				return res.status(400).json({ statusCode: 400, message: 'Error in the database' });
+			}
+			const sqlRes = results[0];
+			if (sqlRes[0]) {
+				const userF = {
+					firstName: sqlRes[0].PrimerNombre,
+					lastName: sqlRes[0].PrimerApellido,
+					password: 'secret',
+					token: 'token',
+				};
+				return res.json(userF);
+			}
+			// return res.status(200).send({
+			// 	success: 1,
+			// 	data: results
+			//
+			// });
+			return res.status(400).json({ statusCode: 400, message: `the password does not match the User: ${data.user1}`});
+		});
 	});
+
 };
--- data/user.js
+++ data/user.js
@@ -1,37 +1,46 @@
-const sql = require("./database");
+const sql = require('./database');
 
 // constructor
-const User = function(user) {
-    this.nombreusuario = user.nombreusuario;
-    this.claveusuario = user.claveusuario;
-    this.estado = user.estado;
+const User = function (user) {
+	this.nombreusuario = user.nombreusuario;
+	this.claveusuario = user.claveusuario;
+	this.estado = user.estado;
 };
 
 //SELECT * FROM usuario
-User.List = result => {
-    sql.query("CALL sp_listado_usuario", (err, res) => {
-        if (err) {
-            console.log("error: ", err);
-            result(null, err);
-            return; 
-        }
+User.List = (result) => {
+	sql.query('CALL sp_listado_usuario', (err, res) => {
+		if (err) {
+			console.log('error: ', err);
+			result(null, err);
+			return;
+		}
 
-        console.log("Usuario: ", res);
-        result(null, res);
-    });
+		console.log('Usuario: ', res);
+		result(null, res);
+	});
+};
+
+User.Find = (data, result) => {
+	sql.query('CALL existe_usuario(?)', [data.user1], (err, res, fields) => {
+		if (err) {
+			console.log('error:', err);
+			result(null, err);
+			return;
+		}
+		result(null, res);
+	});
 };
 
 User.Autenticate = (data, result) => {
-    //sql.query("SELECT idUsuario,NombreUsuario,PrimerNombre,PrimerApellido FROM usuario where nombreusuario = ? AND ClaveUsuario= ?", [data.user1, data.pwd1],
-    sql.query("CALL sp_validar_usuario(?,?)", [data.user1, data.pwd1],
-        (err, res) => {
-            if (err) {
-                console.log("error: ", err);
-                result(null, err);
-                return;
-            }
-            result(null, res);
-        });
+	sql.query('CALL sp_validar_usuario(?,?)', [data.user1, data.pwd1], (err, res, fields) => {
+		if (err) {
+			console.log('error: ', err);
+			result(null, err);
+			return;
+		}
+		result(null, res);
+	});
 };
 
 module.exports = User;
